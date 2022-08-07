b598836fee948fb7e34669522d193dac8914c745
Juan David Rosero Torres <jdrosero59@gmail.com>
2022-08-06 19:09:02 -0500

    Correccion Controllers

--- Modulo UnidadDeportiva/Controllers/DirectorController.cs
+++ Modulo UnidadDeportiva/Controllers/DirectorController.cs
@@ -7,21 +7,23 @@ namespace Modulo_UnidadDeportiva.Controllers
     public class DirectorController : Controller
     {
         private readonly IReportes _reportes;
+        private readonly IAutenticacion _auth;
         private Empleado _empleado;
-        public DirectorController(IReportes reportes)
+        public DirectorController(IReportes reportes, IAutenticacion auth)
         {
             _reportes = reportes;
+            _auth = auth;
         }
 
-        public IActionResult Index(Empleado? empleado)
+        public IActionResult Index()
         {
             if (HttpContext.Session.GetInt32("_id") == null || HttpContext.Session.GetInt32("_id") == -1 
                 || HttpContext.Session.GetInt32("_id") != 2)
             {
                 return RedirectToAction("Index", "Home");
             }
-            _empleado = empleado;
-            return View(empleado);
+            
+            return View(_auth.IngresarDirector((int)HttpContext.Session.GetInt32("_id")));
         }
 
         /*
