9713e84ef8abe475217ca35b9cc82aced5a594fa
YeimerSN <yeimer.serrano@gmail.com>
2022-08-07 20:09:42 -0500

    Solución problema.

--- Modulo UnidadDeportiva/services/AutenticacionService.cs
+++ Modulo UnidadDeportiva/services/AutenticacionService.cs
@@ -55,11 +55,12 @@ namespace Modulo_UnidadDeportiva.services
                 {
                     con.Open();
                     command.Connection = con;
+                    command.BindByName = true;
                     command.CommandText = "select E.codempleado, E.nomempleado, E.apellempleado, C.descargo cargo, " +
                         "E.nomespacio sede from empleado E, empleado_cargo EC, espacio E, cargo C where EC.codempleado = E.codempleado " +
                         "and EC.codempleado='"+codigo+"' and EC.idcargo=C.idcargo and EC.idcargo='3' and " +
                         "EC.codespacio=E.codespacio";
-                    command.CommandType = System.Data.CommandType.Text;
+                    //command.CommandType = System.Data.CommandType.Text;
                     OracleDataReader reader = command.ExecuteReader();
                     var empleado = new Empleado();
                     while (reader.Read())
